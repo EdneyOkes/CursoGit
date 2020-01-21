2db6f386832c9496f5e8e132dd2036025882dfef
Edney <edney.lucas11@yahoo.com.br>
2020-01-21 08:46:16 -0300

    Adicionado teste de modificar commit mantendo alteracoes

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -14,6 +14,7 @@ namespace ProjetoTeste
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Bem vindo de volta das férias!");
             Console.WriteLine("Vamos avançar nessa porra!"); // Voltar ao estado anterior do commit -> git clean -df   -> git checkout -- .
+            Console.WriteLine("Teste de modificar commit agora");
         }
     }
 }
