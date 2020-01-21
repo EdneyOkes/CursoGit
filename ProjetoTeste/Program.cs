cda310ee06fb686a47ed2f8ae6d42fe08d342bae
Edney <edney.lucas11@yahoo.com.br>
2020-01-21 08:53:50 -0300

    Aula removendo commit

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -14,7 +14,13 @@ namespace ProjetoTeste
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Bem vindo de volta das férias!");
             Console.WriteLine("Vamos avançar nessa porra!"); // Voltar ao estado anterior do commit -> git clean -df   -> git checkout -- .
+
             Console.WriteLine("Teste de modificar commit agora");
+
+            // Removendo o commit totalmente e voltando ao estado anterior (git reset --soft HEAD~1) - mantenado os arquivos
+            // Removendo o commit totalmente e voltando ao estado anterior (git reset --hard HEAD~1) - apagando os arquivos
+
+
         }
     }
 }
